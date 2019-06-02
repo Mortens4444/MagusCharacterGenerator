7d2e044ed0571c6add7cbb526872254738a2668c
Mortens4444 <mortens.4444@gmail.com>
2019-06-02 15:43:03 +0200

    Rewrite serialization tests to use memory instead of files

--- MagusCharacterGenerator.Test/ObjectSerializerTest.cs
+++ MagusCharacterGenerator.Test/ObjectSerializerTest.cs
@@ -11,62 +11,50 @@ namespace MagusCharacterGenerator.Test
 		[Test]
 		public void SerializePrimitives()
 		{
-			var primitiveContainer = new
-			{
-				Byte = (byte)255,
-				Short = (short)-2,
-				UShort = (ushort)1,
-				Int32 = (int)-1,
-				UInt32 = (uint)2,
-				Int64 = (long)-100000000000,
-				UInt64 = (ulong)100000000000,
-				Single = (float)3.14,
-				Double = (double)2.71,
-				String = "str",
-				Char = 'c'
-			};
-			var expectedSerializationContent = EmbeddedResourceReader.Get("MagusCharacterGenerator.Test.ObjectSerializationResults.Primitives.json");
-			TestSerialization(primitiveContainer, expectedSerializationContent);
+			TestSerialization(new
+				{
+					Byte = (byte)255,
+					Short = (short)-2,
+					UShort = (ushort)1,
+					Int32 = (int)-1,
+					UInt32 = (uint)2,
+					Int64 = (long)-100000000000,
+					UInt64 = (ulong)100000000000,
+					Single = (float)3.14,
+					Double = (double)2.71,
+					String = "str",
+					Char = 'c'
+				},
+				"MagusCharacterGenerator.Test.ObjectSerializationResults.Primitives.json");
 		}
 
 		[Test]
 		public void SerializeArrays()
 		{
-			var arrayContainer = new
-			{
-				Bytes = new byte[] { 1, 2, 3 },
-				Strings = new[] { "a", "b", "c" }
-			};
-			var expectedSerializationContent = EmbeddedResourceReader.Get("MagusCharacterGenerator.Test.ObjectSerializationResults.Arrays.json");
-			TestSerialization(arrayContainer, expectedSerializationContent);
+			TestSerialization(new
+				{
+					Bytes = new byte[] { 1, 2, 3 },
+					Strings = new[] { "a", "b", "c" }
+				},
+				"MagusCharacterGenerator.Test.ObjectSerializationResults.Arrays.json");
 		}
 
 		[Test]
 		public void SerializeLists()
 		{
-			var listContainer = new
-			{
-				Bytes = new List<byte> { 1, 2, 3 },
-				Strings = new List<string> { "a", "b", "c" }
-			};
-			var expectedSerializationContent = EmbeddedResourceReader.Get("MagusCharacterGenerator.Test.ObjectSerializationResults.Lists.json");
-			TestSerialization(listContainer, expectedSerializationContent);
+			TestSerialization(new
+				{
+					Bytes = new List<byte> { 1, 2, 3 },
+					Strings = new List<string> { "a", "b", "c" }
+				},
+				"MagusCharacterGenerator.Test.ObjectSerializationResults.Lists.json");
 		}
 
-		private void TestSerialization(object obj, string expectedResult)
+		private void TestSerialization(object obj, string serializationResultFileName)
 		{
-			string actualSerializationContent;
-			var filePath = Path.Combine(Path.GetTempPath(), Path.GetTempFileName());
-			try
-			{
-				ObjectSerializer.SaveFile(filePath, obj);
-				actualSerializationContent = File.ReadAllText(filePath);
-			}
-			finally
-			{
-				File.Delete(filePath);
-			}
-			Assert.AreEqual(expectedResult, actualSerializationContent);
+			var expectedSerializationContent = EmbeddedResourceReader.Get(serializationResultFileName);
+			var actualSerializationContent = ObjectSerializer.GetSerializedString(obj);
+			Assert.AreEqual(expectedSerializationContent, actualSerializationContent);
 		}
 	}
 }
