1d6565382adad5d477c1dc9c9e5ee1d9b49ba7af
Mortens <mortens.4444@gmail.com>
2025-12-12 13:42:59 +0100

    Keen hearing/seeing shows the multiplier also (ex. 2x)

---
+++ M.A.G.U.S.Assistant/Converters/SpecialQualificationNameConverter.cs
@@ -0,0 +1,36 @@
+﻿using M.A.G.U.S.GameSystem.Qualifications;
+using M.A.G.U.S.Qualifications.Specialities;
+using Mtf.LanguageService.MAUI;
+using System.Globalization;
+
+namespace M.A.G.U.S.Assistant.Converters;
+
+internal class SpecialQualificationNameConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is KeenHearing kh)
+        {
+            return $"{Lng.Elem(kh.Name)} ({kh.Multiplier:F1}x)";
+        }
+        else if (value is KeenSight ks)
+        {
+            return $"{Lng.Elem(ks.Name)} ({ks.Multiplier:F1}x)";
+        }
+        else if (value is KeenSmell k)
+        {
+            return $"{Lng.Elem(k.Name)} ({k.Multiplier:F1}x)";
+        }
+        else if (value is SpecialQualification sq)
+        {
+            return Lng.Elem(sq.Name);
+        }
+
+        return String.Empty;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value;
+    }
+}
