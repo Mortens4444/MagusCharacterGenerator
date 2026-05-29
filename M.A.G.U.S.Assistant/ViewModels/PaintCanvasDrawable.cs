384b2908b3747e3fce37ea89abc68ac8495a0c60
Mortens <mortens.4444@gmail.com>
2026-05-30 01:00:56 +0200

    Delete unused code

--- M.A.G.U.S.Assistant/ViewModels/PaintCanvasDrawable.cs
+++ M.A.G.U.S.Assistant/ViewModels/PaintCanvasDrawable.cs
@@ -1,5 +1,4 @@
 ﻿using M.A.G.U.S.Assistant.Interfaces;
-using M.A.G.U.S.Assistant.Models.Drawing;
 
 namespace M.A.G.U.S.Assistant.ViewModels;
 
@@ -35,32 +34,6 @@ internal sealed class PaintCanvasDrawable : IDrawable
         }
 
         element.Draw(canvas);
-        canvas.RestoreState();
-    }
-
-    private void DrawSelectionHighlight(ICanvas canvas, IDrawableElement element)
-    {
-        var center = element.GetCenter();
-
-        // Elforgatjuk a kijelölő keretet is, ha az elem el van forgatva
-        canvas.SaveState();
-        canvas.Rotate(element.Rotation, center.X, center.Y);
-
-        canvas.StrokeColor = Colors.DeepSkyBlue;
-        canvas.StrokeSize = 1;
-        canvas.StrokeDashPattern = new float[] { 4, 4 }; // Szaggatott vonal
-
-        // Itt egy becsült téglalapot rajzolunk az elem köré
-        // A pontos mérethez az element-nek kellene visszaadnia egy Rect-et
-        if (element is RectangleElement re)
-        {
-            canvas.DrawRectangle(((RectangleElement)element).Rect.Inflate(5, 5));
-
-            canvas.FillColor = Colors.White;
-            canvas.FillEllipse(re.Rect.X - 8, re.Rect.Y - 8, 16, 16);
-            canvas.DrawEllipse(re.Rect.X - 8, re.Rect.Y - 8, 16, 16);
-        }
-
         canvas.RestoreState();
     }
 }
